b7e1d218c6383e91ad0a43108e698c8d941fb0ea
aminmohammadmazeali <aminmohammadmazreali@gmail.com>
2017-08-03 21:44:49 +0430

    Ready to run on heroku

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Telegram\Bot\Api;
+
+class ApiController extends Controller
+{
+    public function me(){
+        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
+        $response = $telegram->getMe();
+        return $response;
+    }
+
+    public function updates()
+    {
+        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
+        $response = $telegram->getUpdates();
+        return $response;
+    }
+
+    public function respond(Request $request){
+        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
+        $response = $telegram->getUpdates();
+        $request = collect(end($response));
+
+        $chatid = $request['message']['chat']['id'];
+        $text = $request['message']['text'];
+
+        switch($text) {
+            case '/start':
+                $this->showMenu($telegram, $chatid);
+                break;
+            case '/menu':
+                $this->showMenu($telegram, $chatid);
+                break;
+            case '/website':
+                $this->showWebsite($telegram, $chatid);
+                break;
+            case '/contact';
+                $this->showContact($telegram, $chatid);
+                break;
+            default:
+                $info = 'I do not understand what you just said. Please choose an option';
+                $this->showMenu($telegram, $chatid, $info);
+        }
+
+    }
+
+    public function showMenu($telegram, $chatid, $info = null){
+        $message = '';
+        if($info !== null){
+            $message .= $info.chr(10);
+        }
+        $message .=  '/website'.chr(10);
+        $message .= '/contact'.chr(10);
+
+        $response = $telegram->sendMessage([
+            'chat_id' => $chatid,
+            'text' => $message
+        ]);
+    }
+
+    public function showWebsite($telegram, $chatid){
+        $message = 'http://google.com';
+
+        $response = $telegram->sendMessage([
+            'chat_id' => $chatid,
+            'text' => $message
+        ]);
+    }
+
+    public function showContact($telegram, $chatid){
+        $message = 'info@jqueryajaxphp.com';
+
+        $response = $telegram->sendMessage([
+            'chat_id' => $chatid,
+            'text' => $message
+        ]);
+    }
+
+    public function setwebhook(Request $request){
+        $telegram = new Api(env('TELEGRAM-BOT-TOKEN'));
+        $server=$_SERVER['HTTP_HOST'];
+        $response = $telegram->setWebHook(["url" => "https:///my-bot-token/webhook"]);
+
+        return $response;
+//
+//
+//        $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
+//        return $request['chat'];
+//        $chatid = $request['message']['chat']['id'];
+//        $text = $request['message']['text'];
+//
+//        switch($text) {
+//            case '/start':
+//                $this->showMenu($telegram, $chatid);
+//                break;
+//            case '/menu':
+//                $this->showMenu($telegram, $chatid);
+//                break;
+//            case '/website':
+//                $this->showWebsite($telegram, $chatid);
+//                break;
+//            case '/contact';
+//                $this->showContact($telegram, $chatid);
+//                break;
+//            default:
+//                $info = 'I do not understand what you just said. Please choose an option';
+//                $this->showMenu($telegram, $chatid, $info);
+//        }
+    }
+}
+
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('me', 'ApiController@me');
+
+Route::get('updates', 'ApiController@updates');
+Route::get('respond', 'ApiController@respond');
+
+Route::get('setWebHook', 'ApiController@setWebHook');
+
+Route::post('my-bot-token/webhook', 'ApiController@setWebHook');
+
+
+
+
+
+
+
